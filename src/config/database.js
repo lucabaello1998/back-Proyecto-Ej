d2c02ccbfdad91628b4fe80973a549da7772d328
Luca Nicolas Baello <luca.baello@gmail.com>
2025-11-17 22:58:55 -0300

    agregado env

--- src/config/database.js
+++ src/config/database.js
@@ -11,6 +11,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
 // Verificar conexión
